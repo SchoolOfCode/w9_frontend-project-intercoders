bca6925a1f5db7cb4b5b436b4bfddd260358f9ef
eda <edaburns@gmail.com>
2022-06-24 10:57:48 +0100

    first React accessibility test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import { test, expect } from "@jest/globals";
 import App from './App';
+import Register from "./components/Register/Register.js"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+test('renders register', () => {
+  
+  render(<Register/>);
+  
+  // const onClick = jest.fn()
+   const button = screen.getByRole("button", {name: "Register"});
+   
+   expect(button).toBeInTheDocument()
+  //  screen.debug()
+  //const button = screen.getByText("Add To List")
+ });
+
+
+ 
